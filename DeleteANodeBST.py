8d98f007ff32325b4fdd38a796231d6838d37a4d
Sudharssen Murali <m.sudharssen@gmail.com>
2026-01-28 22:44:58 -0500

    Delete Node BST

---
+++ DeleteANodeBST.py
@@ -0,0 +1,25 @@
+"""
+2 cases:
+1. No child, return root
+"""
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left :
+                return root.right
+            if not root.right:
+                return root.left
+            else:
+                curr = root.right
+                while curr.left:
+                    curr = curr.left
+                root.val = curr.val
+                root.right = self.deleteNode(root.right, root.val)
+        return root
